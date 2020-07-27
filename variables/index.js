7a1ab12465977c117132fc46c0466b0839ddad89
Jacob <jacob.reilly.cooper@gmail.com>
2020-07-27 16:37:14 +0100

    day one

---
+++ variables/index.js
@@ -0,0 +1,118 @@
+// * created folder called variables
+// * inserted a file called index.js inside of that folder
+
+// * index.js entry to the program that we are making
+// * index.js OR app.js
+
+// * I can write javascript to complete a problem I'm having.
+
+// * variable = a storage place that can change
+// * myName = "Jacob";
+
+// * variables, if statements, functions, arrays and loops...
+
+// let myName = "Jacob Reilly-Cooper"; // let myName be equal to Jacob
+// myName = "Steve"; // string
+let age = 55; // number
+// data types
+//* string = string of text - name, address, email, grocery list
+//* number = age, height, weight, quantity, ratings
+//* boolean = true OR false, on OR off, yes OR no
+
+/** 
+ * create a few variables about yourself.
+ * 4 variables - numeric value or simply a string
+ */
+
+// let myName = "Hamza Mahmood";
+// * declaration: let myName;
+// * definition: "Hamza Mahmood"
+let hamzaAge = 19;
+let hamzaHeight = "5'11";
+let hamzaHobbies = "Cars, eating, gaming";
+
+myName = "Danny"; // * defined the variable. Changed the value of myName
+let dannyAge = 21;
+let dannyHeight = "4'6";
+let dannyHobbies = "golf, football, coding";
+// const bloodType;
+
+// console.log(myName);
+// * variables can change
+// * constant cannot change
+const dateOfBirth = "01.01.1990";
+// dateOfBirth = "cheese";
+// console.log(dateOfBirth);
+
+//* travelling to work before COVID...
+// * Alex's journey to Australia
+let modeOfTransport = "Train"; // replacement bus service
+let timeTaken = 30; // minutes // depending on different factors
+let route = "New Town, Town Hall"; // varied route - fast
+let weather = "Very... pause... hot."; // rained sometimes
+let kangaroo = false; // once or twice
+let paceToTrain = "fast"; // casually walked every now and then
+const endPosition = "Work"; // our only constant
+// endPosition = "Home" // ! TypeError: Assignment to constant variable.
+// ? change const to a let if you want to reassign
+
+let startingPosition = "     Home      "; // changed
+// console.log(startingPosition.trimStart());
+let alexNumber = 36;
+let myNumber = 0;
+// console.log(alexNumber = myNumber + 10) // 0 + 10
+console.log(alexNumber)
+let attempt = alexNumber.toString()
+console.log(attempt)
+attempt = parseInt(attempt)
+console.log(attempt)
+// ? < > <= >= == ===
+
+// console.log(alexNumber);
+// alexNumber = alexNumber * 4 // common practice
+// alexNumber *= 4 // best practice
+// alexNumber **= 4
+// alexNumber = alexNumber / 4
+// alexNumber /= 4 // alexNumber = alexNumber + 4
+//   + - / *
+//   += -= /= *=
+// console.log(alexNumber) // 
+
+
+// +, -, /, *, +=, -=, /=, *=
+// use all of the above operators
+// console.log(myNumber);
+//* node variables/index.js
+// ? OR 
+//* cd variables
+//* node index.js
+
+// console.log(0.2 + 0.1)
+
+// const myNameFunction = (myName) => {
+
+//     console.log("Hello, " + myName)
+// }
+
+const addition = (numberOne, numberTwo) => {
+    console.log("My first number is: " + numberOne)
+    console.log("My second number is: " + numberTwo)
+    console.log(numberOne + numberTwo)
+}
+
+addition(36, 13)
+addition(8, 7)
+addition(80, 70)
+
+
+
+
+
+// myNameFunction("Jacob")
+// myNameFunction("Sam")
+// myNameFunction("Alex")
+// myNameFunction("Danny")
+// myNameFunction("Donna")
+// myNameFunction("Janna")
+// myNameFunction("Silvia")
+// myNameFunction("Hamza")
