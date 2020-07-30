6fd6805bc593c7066309f24f2c797d2a218f17b9
Jacob <jacob.reilly.cooper@gmail.com>
2020-07-30 09:21:25 +0100

    challenges work uploaded

---
+++ challenges/index.js
@@ -0,0 +1,9 @@
+//? Create a function that takes a number as a parameter and subtracts one from that number.
+
+//? Create a function that takes two numbers as a parameters and checks if the first one is greater than the second one.
+
+//? Create a function that takes two numbers as a parameters and checks if they are equal.
+
+//? Create a car park system that will log how many hours you stayed at the car park and charge you by the hourly price. 
+//? If you have stayed for 8 hours, you get the day rate. 
+//? If you are staff, you get a staff discount
