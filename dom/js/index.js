b1a3556850f5e9c96d017832317c4f2c99153df5
Jacob <jacob.reilly.cooper@gmail.com>
2020-08-10 16:19:25 +0100

    querySelector & QSAll code with challenge

--- dom/js/index.js
+++ dom/js/index.js
@@ -11,47 +11,69 @@
 //* click, an enter, a hover, a highlight... anything that user can do, the dom can do better. the dom can do anything better than youSER.
 
 //* variables and constants 
-const myHeading = document.getElementById("myHeading");
+// const myHeading = document.getElementById("myHeading");
+const myHeading = document.querySelector("#myHeading")
 const input = document.getElementById("input");
 const button = document.getElementById("button");
 
-const purpleParagraphs = document.getElementsByClassName("purple")
+// const purpleParagraphs = document.getElementsByClassName("purple")
+const purpleParagraphs = document.querySelectorAll(".purple") //? #IDs
 const notPurpleParagraphs = document.getElementsByClassName("notPurple")
 const paragraphs = document.getElementsByTagName("p");
 //?                pay close attention to the .getElements
 
+
+const theLi = document.querySelectorAll("li");
+
 //* functions
-myHeading.addEventListener("click", ()=> {
-    console.log(`The paragraphs length is: ${paragraphs.length}`)
-    
-})
-
-
+const colours = ['red', 'yellow', 'goldenrod', 'lightgreen', 'blue', 'purple', 'orange'];
+for (let i = 0; i < theLi.length; i++) {
+    theLi[i].style.color = colours[i];
+    //? i is being incremented. So, i will only get to the value of theLi.length
+}
 
 button.addEventListener("click",()=> {
-    //? the heading will change color to the input box's value
-    // myHeading.style.color = input.value;
-    //* targeted like an array, the first index of the paragraphs 
-    // paragraphs[0].style.color = input.value;
-    // for (let i = 0; i < paragraphs.length; i++){
-    //     paragraphs[i].style.color = input.value;
-    // }
-
-    // for(i = 0; i < purpleParagraphs.length; i++) {
-    //     purpleParagraphs[i].style.color = input.value;
-    // }
-    for(i = 0; i < notPurpleParagraphs.length; i++) {
-        notPurpleParagraphs[i].style.color = "orange";
-    }
     for (const paragraph of purpleParagraphs) {
-        paragraph.style.color = input.value;
+        paragraph.style.color = "red"
     }
-    
+    for (let i = 0; i < theLi.length; i++) {
+        theLi[i].style.color = "red";
+    }
 })
+
+
+
+// button.addEventListener("click",()=> {
+//     //? the heading will change color to the input box's value
+//     // myHeading.style.color = input.value;
+//     //* targeted like an array, the first index of the paragraphs 
+//     // paragraphs[0].style.color = input.value;
+//     // for (let i = 0; i < paragraphs.length; i++){
+//     //     paragraphs[i].style.color = input.value;
+//     // }
+
+//     // for(i = 0; i < purpleParagraphs.length; i++) {
+//     //     purpleParagraphs[i].style.color = input.value;
+//     // }
+//     for(i = 0; i < notPurpleParagraphs.length; i++) {
+//         notPurpleParagraphs[i].style.color = "orange";
+//     }
+//     for (const paragraph of purpleParagraphs) {
+//         paragraph.style.color = input.value;
+//     }
+    
+// })
 
 //! getElement & getElements
 //? we can target the element normally with getElement
 //? we have to target a particular index with getElements
 //? purpleParagraphs[0] || purpleParagraphs[i] for getElements
 
+
+
 // TODO: challenge to go here.
+// TODO: display longer than 6 li's and have them restart the colour cycle.
+
+// TODO: 6 li's... and for you to loop through using this array of colours.
+//? const colours = ['red', 'yellow', 'goldenrod', 'lightgreen', 'blue', 'purple'];
+
