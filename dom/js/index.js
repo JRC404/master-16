68875b0070378eef8a79df99be497bbfdba7620a
Jacob <jacob.reilly.cooper@gmail.com>
2020-08-10 12:19:55 +0100

    getElementById, getElementsByClassName, getElementsByTagName

--- dom/js/index.js
+++ dom/js/index.js
@@ -10,5 +10,48 @@
 //* doing something when a user wants us to
 //* click, an enter, a hover, a highlight... anything that user can do, the dom can do better. the dom can do anything better than youSER.
 
+//* variables and constants 
+const myHeading = document.getElementById("myHeading");
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+
+const purpleParagraphs = document.getElementsByClassName("purple")
+const notPurpleParagraphs = document.getElementsByClassName("notPurple")
+const paragraphs = document.getElementsByTagName("p");
+//?                pay close attention to the .getElements
+
+//* functions
+myHeading.addEventListener("click", ()=> {
+    console.log(`The paragraphs array length is: ${paragraphs.length}`)
+    
+})
 
 
+
+button.addEventListener("click",()=> {
+    //? the heading will change color to the input box's value
+    // myHeading.style.color = input.value;
+    //* targeted an array, the first index of the paragraphs array
+    // paragraphs[0].style.color = input.value;
+    // for (let i = 0; i < paragraphs.length; i++){
+    //     paragraphs[i].style.color = input.value;
+    // }
+
+    for(i = 0; i < purpleParagraphs.length; i++) {
+        purpleParagraphs[i].style.color = input.value;
+    }
+    for(i = 0; i < notPurpleParagraphs.length; i++) {
+        notPurpleParagraphs[i].style.color = "orange";
+    }
+    for (const paragraph of purpleParagraphs) {
+        console.log(paragraph)
+    }
+    
+})
+
+//! getElement & getElements
+//? we can target the element normally with getElement
+//? we have to target a particular index with getElements
+//? purpleParagraphs[0] || purpleParagraphs[i] for getElements
+
+// TODO: challenge to go here.
