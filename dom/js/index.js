0591711baeb034ef00658a5a9ddc596e46ef9b6a
Jacob <jacob.reilly.cooper@gmail.com>
2020-08-10 11:05:56 +0100

    javascript set up, html used.

---
+++ dom/js/index.js
@@ -0,0 +1,14 @@
+//? DOM - Document Object Model
+//? JS make the DOM alter a website / webpage
+//? global object - represents the content on the page, our html.
+
+//* select elements of a page that is already loaded and modify them.
+//* change paragraph's color... we can do that with JS.
+
+//* select elements
+//* manipulating elements
+//* doing something when a user wants us to
+//* click, an enter, a hover, a highlight... anything that user can do, the dom can do better. the dom can do anything better than youSER.
+
+
+
